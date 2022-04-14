8c247c450704976fd46f7a140d21ddd029034a08
rafaelmaia23 <rafaelmaiadafonseca@gmail.com>
2022-04-14 10:32:35 -0300

    Model validations, get and get by id for videos
    Updated Video Model with more dataNotations for validation,
    Added Video Contontroller and Video Service
    Added Http Get metod for videos (controller and service)
    Added Http get metod by Id for videos (controller and service)

---
+++ VideosApi/VideosApi/Controllers/VideosController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VideosApi.Models;
+using VideosApi.Services;
+
+namespace VideosApi.Controllers
+{
+    [ApiController]
+    [Route("controller")]
+    public class VideosController : ControllerBase
+    {
+        private readonly VideosService _videosService;
+
+        public VideosController(VideosService videosService)
+        {
+            _videosService = videosService;
+        }
+
+        [HttpGet]
+        public IActionResult ReadVideo()
+        {
+            List<Video> videoList = _videosService.ReadVideo();
+            if (videoList == null) return NotFound();
+            return Ok(videoList);
+        }
+    }
+}
---
+++ VideosApi/VideosApi/Services/VideosService.cs
@@ -0,0 +1,22 @@
+﻿using VideosApi.Data;
+using VideosApi.Models;
+
+namespace VideosApi.Services
+{
+    public class VideosService
+    {
+        private readonly AppDbContext _db;
+
+        public VideosService(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public List<Video> ReadVideo()
+        {
+            List<Video> videoList = _db.Videos.ToList();
+            if(videoList == null) return null;
+            return videoList;
+        }
+    }
+}
